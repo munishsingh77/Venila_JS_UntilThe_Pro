e29f2cea2f03fdbb810b4d3adabe2345f9e5533b
munishsingh77 <you@example.com>
2025-07-13 13:49:26 +0530

    Greeting App

---
+++ Greeting_App/Greeting_app.js
@@ -0,0 +1,25 @@
+// Build a *"Greeting App"* — when you click a button, 
+// it says “Hello Munish!”
+//
+
+
+
+
+
+
+const GreetingButton = document.querySelector(".greet-btn");
+const GreetingText = document.querySelector(".greet-Text");
+
+console.log(GreetingButton);
+console.log(GreetingText);
+
+GreetingButton.addEventListener('click', function () {
+    
+    GreetingText.textContent = "Hello Munish";
+})
+
+GreetingButton.addEventListener('click', function () {
+    
+    alert("Hello Munish");
+})
+
