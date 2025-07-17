04a87591f2149553868ff5d41b74b8b6af175ba5
munishsingh77 <you@example.com>
2025-07-17 20:26:47 +0530

    Age Calculator

--- 02Age_Calcualator/Age_Calculator.js
+++ 02Age_Calcualator/Age_Calculator.js
@@ -1,6 +1,32 @@
 // ### 2. *Age Calculator*
 
-// > 👉 Ask user for birth year → calculate current age
+// > 👉 Ask user for Date of birth → calculate current age
+//   👉 So i have to subtract the date of birth from current date
 
-const user = "";
-const BirthYear = "";
+
+
+
+// I have the Button
+const calculateAge =  document.querySelector("#Calculate-Age");
+ 
+function toGetcurrentAge() {
+    const dateOfbirth = document.querySelector("#Birth-Date").value;
+    const userDate = new Date(dateOfbirth);
+    const year = userDate.getMilliseconds();
+    
+    
+    const today = new Date();
+    const day = today.getMilliseconds();
+    
+     currentAge = day - year ;
+    console.log(currentAge);
+
+   const exactAge = document.querySelector("label");
+     exactAge.textContent = "My Age is : " + currentAge;
+} 
+
+calculateAge.addEventListener("click", toGetcurrentAge);
+
+
+
+// Subtraction function
